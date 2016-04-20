4f0a4b8dee98e0f6c2854306dd9ceda847f2270b
Stephen Mayeux <stephenmayeux@gmail.com>
2016-04-21 02:07:16 +0900

    finish game

--- script.rb
+++ script.rb
@@ -1,36 +1,73 @@
 class TowerOfHanoi
   ## Class Variables
   @@messages = {
-    new_game: "Welcome to the Tower of Hanoi!\nInstructions:\nEnter where you'd like to move from and to\nin the format [1,3]. Enter 'q' to quit.",
-    wrong_input: "You have entered invalid input.\nEnter where you'd like to move from and to\nin the format [1,3]. Enter 'q' to quit.",
+    new_game: "Welcome to the Tower of Hanoi!",
+    prompt_user: "Enter where you'd like to move from and to\nin the format [1,3]. Enter 'q' to quit.",
     winner: "Congratulations! You have won the game!",
     quit: "You have quit the game.\nHope to see you again!",
-    illegal_move: "You can't add a bigger piece atop a smaller piece.\nPlease try again."
+    illegal_move: "You can't add a bigger piece atop a smaller piece.\nPlease try again.",
+    starting_message: "How many pieces would you like in your Tower of Hanoi?"
   }
-  @@game_over = false
 
-  def render_game_grid
-    puts "this is a grid!"
+  attr_reader :towers, :complete_tower
+  attr_accessor :counter
+
+  def initialize(pieces)
+    @complete_tower = pieces.downto(1).to_a
+    @towers = (1..3).each_with_object({}) { |num, tower| tower[num] = [] }
+    @towers[1] = @complete_tower.dup
+    @counter = 0
   end
 
-  def get_input
-    @input = gets.chomp
-    if @input.downcase == 'q' || 'quit'
-      puts @@messages[:quit]
-      @@game_over = true
+  def move(from, to)
+    if !towers.include?(from)
+      puts "Tower #{from} doesn't exist. Try again."
+    elsif !towers.include?(to)
+      puts "Tower #{to} doesn't exist. Try again."
+    elsif from.equal?(to)
+      puts "You have selected the same tower. Try again."
+    elsif tower(to).any? && tower(to).last < tower(from).last
+      puts @@messages[:illegal_move]
+    else
+      tower(to) << tower(from).pop
+      self.counter += 1
     end
   end
 
-  def start_game_loop
-    puts @@messages[:new_game]
-    get_input
-    until @@game_over
-      break
-    end
+  def over?
+    complete_tower == tower(2) || complete_tower == tower(3)
   end
 
-  def start(number_of_pieces)
-    render_game_grid
-    start_game_loop
+  def tower(number)
+    towers[number]
   end
+
+  def self.messages
+    @@messages
+  end
+
 end
+
+puts TowerOfHanoi.messages[:starting_message]
+pieces = gets.chomp.to_i
+min_steps = (2 ** pieces) - 1
+game = TowerOfHanoi.new(pieces)
+puts TowerOfHanoi.messages[:new_game]
+
+until game.over?
+  game.towers.each { |tower, piece| puts "Tower: #{tower}: #{piece}"}
+
+  puts TowerOfHanoi.messages[:prompt_user]
+  input = gets.chomp
+  if input.downcase == "q" || input.downcase == "quit"
+    TowerOfHanoi.messages[:quit]
+    break
+  end
+
+  directions = input.split("").collect(&:strip)
+
+  game.move(directions[1].to_i, directions[3].to_i)
+end
+
+puts TowerOfHanoi.messages[:winner]
+puts "You beat the game in #{game.counter} steps.\nIt takes a minimum of #{min_steps} steps to beat the game."
